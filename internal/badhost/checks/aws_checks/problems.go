16338e6b4ef5ebf73cc8ffd555c55636dc22a350
nabutabu <nabu.tabu2402@gmail.com>
2026-01-15 11:43:16 -0800

    detection init

--- internal/badhost/checks/aws_checks/problems.go
+++ internal/badhost/checks/aws_checks/problems.go
@@ -3,11 +3,13 @@ package aws_checks
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/nabutabu/crane-oss/internal/badhost/problem"
 	"github.com/nabutabu/crane-oss/pkg/api"
 )
 
 type UnhealthyEC2Instance struct {
+	client *ec2.Client
 }
 
 func (c *UnhealthyEC2Instance) Name() string {
@@ -15,5 +17,16 @@ func (c *UnhealthyEC2Instance) Name() string {
 }
 
 func (ec2Instance *UnhealthyEC2Instance) Detect(ctx context.Context, host *api.Host) ([]problem.Problem, error) {
+	input := &ec2.DescribeInstanceStatusInput{
+		InstanceIds: []string{host.ProviderID},
+		// Optional: IncludeAllInstances can be set to false if only a specific instance is needed
+		IncludeAllInstances: &[]bool{true}[0],
+	}
+
+	_, err := ec2Instance.client.DescribeInstanceStatus(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
